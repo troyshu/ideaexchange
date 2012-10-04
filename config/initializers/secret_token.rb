24c59ca601fb57cf90f4091ae9a894e7bd271fcc
Troy Shu <tmshu1@gmail.com>
2012-10-04 10:06:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideaexchange::Application.config.secret_token = '81b2648d540e007abf7babc592432147e83922a0a577b93ee4e9a1c854b2885add8bc8c0516c5a2fb3b4e2ad0d5c1a71c3e685e6ec9c60011638392b13831f57'
